d3368718aa8fe78d8516f1c69987ca2035229b25
Sean Tauber <seanetauber@gmail.com>
2014-10-26 20:02:19 +1030

    overhauled experiment server to use new psycloud python client

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 import os
 import json
 
-from psycloudclient import PsycloudClient
-psycloud_client = PsycloudClient("http://psycloud-server-1.appspot.com")
+from psycloud_python.client import PsycloudClient
+from psycloud_python.custom_exceptions import DuplicateEntryError, ResourceError
+
+psycloud_client = PsycloudClient("http://psycloud-server-2.appspot.com")
 
 # Import the Flask Framework
 from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
@@ -19,17 +21,25 @@ def coupon_landing(exp_type, experiment_id):
 
 @app.route('/cregister', methods=['POST'])
 def register_with_coupon():
+
 	exp_id = request.form['exp']
 	exp_type = request.form['exp_type']
 	reg_coupon = request.form['reg_coupon']
-	result = psycloud_client.register(exp_id, registration_coupon=reg_coupon)
-	if result['status'] == 200:
-		participant_id = result['result']['participant']['short_id']
-		return render_template('%s/instructions.html'%exp_type, uid=participant_id, exp_type=exp_type)
-	elif result['status'] == 400 and result['result'] == "Duplicate registration coupon":
+
+	try:
+		participant_id = psycloud_client.register(exp_id, registration_coupon=reg_coupon)
+	
+	except DuplicateEntryError:
 		return render_template('%s/duplicate_notify.html'%exp_type)
+	
+	except ResourceError:
+		abort(410)
+	
+	except Exception, e:
+		return bad_request(e)
+	
 	else:
-		return jsonify(result)
+		return render_template('%s/instructions.html'%exp_type, uid=participant_id, exp_type=exp_type)
 
 
 @app.route('/experiment', methods=['POST'])
@@ -37,59 +47,75 @@ def experiment():
 
 	participant_id = request.form['uid']
 	exp_type = request.form['exp_type']
-	# participant = psycloud_client.get_participant(participant_id)
 
-	if "responses" in request.form:
-		form_keys = request.form.keys()
-		resp_keys = []
-		for key in form_keys:
-			s = key.split('_')
-			if s[0] == 'resp':
-				resp_keys.append( [ s[1], int(s[2]), key ] )
-		resp_keys.sort()
-		r = {}
-		for key in resp_keys:
-			name = key[0]
-			if r.has_key(name):
-				r[name].append(request.form[key[2]])
+	current_stim = psycloud_client.get_current_stimulus(participant_id)
+	max_stim = psycloud_client.get_max_stimulus_count(participant_id) - 1
+	r = None # This will contain the previous response variables if they exist but defaults to None
+
+	if current_stim > 0:
+
+		if "responses" in request.form:
+			form_keys = request.form.keys()
+			resp_keys = []
+			for key in form_keys:
+				s = key.split('_')
+				if s[0] == 'resp':
+					resp_keys.append( [ s[1], int(s[2]), key ] )
+			resp_keys.sort()
+			r = {}
+			for key in resp_keys:
+				name = key[0]
+				if r.has_key(name):
+					r[name].append(request.form[key[2]])
+				else:
+					r[name] = [request.form[key[2]]]
+
+			# r now contains a dictionary of response variables... so save it.
+			try:
+				saved_response = psycloud_client.save_response(participant_id, current_stim - 1, r)
+			except:
+				abort(500)
+			
+			if current_stim < max_stim:
+				# increment current stimulus
+				try:
+					success = psycloud_client.set_current_stimulus(participant_id, current_stim + 1)
+				except:
+					abort(500)
+				else:
+					if success:
+						current_stim += 1
+					else:
+						abort(500)
 			else:
-				r[name] = [request.form[key[2]]]
+				# Experiment is over
+				conf_code = psycloud_client.get_confirmation_code(participant_id)
+				return render_template('%s/post_experiment.html'%exp_type, verification_code=conf_code)
 
-		previous_response_variables = r
-	else:
-		previous_response_variables = None
-
-	if previous_response_variables is None:
-		result = psycloud_client.get_current_stimulus(participant_id)
-	
-	else:
-		result = psycloud_client.save_current_response(participant_id, {'variables':previous_response_variables})
-		
-		if result['status'] == 200:
-			result = psycloud_client.increment_and_get_next_stimulus(participant_id)
 		else:
-			# return bad_request("Unable to save to database. Please contact the experiment administrator.")
-			return jsonify(result)
+			# current stimulus is >0 but there was no response data... they probably reloaded
+			# or navigated back to the page. So load the current stimuli without saving a response
+			# and without incrementing the stimulus counter
+			pass
 
-	if result['status'] == 200:
-		stimulus = result['result']['stimuli'][0]
+
+	try:
+		# Get the stimulus
+		stimulus = psycloud_client.get_stimulus(participant_id, current_stim)
+	except:
+		abort(500)
+	else:
+
+		#Extract the stimulus information and render the template
+		
 		stimulus_template = stimulus['stimulus_type'] + '.html'
 		stimulus_variables = stimulus['variables']
+
 		return render_template('%s/%s'%(exp_type, stimulus_template), exp_type=exp_type,
-			uid=participant_id, stim=stimulus_variables, prev=previous_response_variables)
-	else:
-		result = psycloud_client.completed(participant_id)
-		if result['status'] == 200:
-			return render_template('%s/post_experiment.html'%exp_type, verification_code=participant_id)
-		else:
-			return jsonify(result)
+			uid=participant_id, stim=stimulus_variables, prev=r)
 
 
-# @app.route('/test/<exp_type>/<template_type>', methods=['GET'])
-# def instructions_test(exp_type, template_type):
-# 	# exp_type = 'mammals'
-# 	uid = 'part_uid'
-# 	return render_template('%s/%s.html'%(exp_type,template_type), uid=uid, exp_type=exp_type)
+
 
 @app.route('/submitresponse', methods=['POST'])
 def test_response():
@@ -103,6 +129,11 @@ def bad_request(e):
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(410)
+def experiment_full(e):
+    return jsonify( {'status':410, 'message':'Experiment full.'}), 410
 
 
 @app.errorhandler(500)
